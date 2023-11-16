e09313b0840e40091acf39636e0db26150345c8f
Paweł Zaic <pawel.zaic@itcraft.pl>
2023-11-16 19:14:44 +0100

    Storybook Setup | Added icons

---
+++ src/assets/icons/ChevronLeftIcon.tsx
@@ -0,0 +1,20 @@
+export const ChevronLeftIcon = ({ ...props }) => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		{...props}
+	>
+		<path
+			d="M14 18L8 12L14 6L15.4 7.4L10.8 12L15.4 16.6L14 18Z"
+			fill="#AAAAAA"
+		/>
+		<path
+			d="M14 18L8 12L14 6L15.4 7.4L10.8 12L15.4 16.6L14 18Z"
+			fill="black"
+			fill-opacity="0.2"
+		/>
+	</svg>
+);
---
+++ src/assets/icons/icons.stories.tsx
@@ -0,0 +1,32 @@
+import { Box, Stack } from '@mui/system';
+import { theme } from '@web/lib';
+import { Typography } from '@mui/material';
+import * as Icons from './index';
+
+const IconsEntries = Object.entries(Icons);
+
+/**
+ * Story to show all icons that are in form of React components
+ */
+const Story = {
+	component: Icons,
+	title: 'assets/Icons',
+};
+export default Story;
+
+const Template = () => (
+	<Stack direction="column" spacing={3}>
+		{IconsEntries.map((item, index) => (
+			<Box
+				component="div"
+				sx={{ display: 'flex', alignItems: 'center', gap: theme.spacing(4) }}
+				key={index}
+			>
+				<Typography variant="body1"> {item[0]}:</Typography>
+				{item[1]({ fontSize: 'large' })}
+			</Box>
+		))}
+	</Stack>
+);
+
+export const Primary = Template.bind({});
