49be22f86e25577cab956b011433a8374970c761
Paweł Zaic <pawel.zaic@itcraft.pl>
2023-11-18 23:02:00 +0100

    Changed breakpoints

--- src/lib/theme/theme.ts
+++ src/lib/theme/theme.ts
@@ -27,6 +27,15 @@ declare module '@mui/material/styles' {
 }
 
 let theme = createTheme({
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 600,
+			md: 900,
+			lg: 1200,
+			xl: 1920,
+		},
+	},
 	palette: {
 		primary: {
 			main: '#423C66',
