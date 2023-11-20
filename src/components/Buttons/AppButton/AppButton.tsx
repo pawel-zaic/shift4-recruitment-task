25667ad66a9edf6747c3bb6efeda7672a439639c
Paweł Zaic <pawel.zaic@itcraft.pl>
2023-11-20 09:50:16 +0100

    Component | Fixed button incorrect props passing order

--- src/components/Buttons/AppButton/AppButton.tsx
+++ src/components/Buttons/AppButton/AppButton.tsx
@@ -10,11 +10,11 @@ type AppButtonProps = MuiButtonProps & {
 
 export const AppButton = ({ icon = null, children, onClick, ...props }: AppButtonProps) => (
 	<StyledAppButton
-		{...props}
 		color="primary"
 		variant="contained"
 		disableElevation
 		disableRipple
+		{...props}
 		onClick={onClick}
 	>
 		{icon}
